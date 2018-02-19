896add77072b8f1617239622f6f8accce135a651
Kevin Willocks <kevinwillocks@gmail.com>
2018-02-19 17:47:41 -0500

    Added tableview, assets and IBoutlets and actions for lesson 3

--- GoalPostApp/Controller/GoalsVC.swift
+++ GoalPostApp/Controller/GoalsVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GoalVC.swift
 //  GoalPostApp
 //
 //  Created by Kevin Willocks on 2/19/18.
@@ -8,17 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GoalsVC: UIViewController {
 
+    @IBOutlet weak var TableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addGoalButtonWasPressed(_ sender: Any) {
+        print("Button was pressed")
     }
+    
+  
 
 
 }
