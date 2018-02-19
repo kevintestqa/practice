8dad4907c09cd2bf4bb6d75e41d9be40ce24fbc8
Kevin Willocks <kevinwillocks@gmail.com>
2018-02-19 17:02:42 -0500

    Initial Commit

---
+++ GoalPostApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoalPostApp
+//
+//  Created by Kevin Willocks on 2/19/18.
+//  Copyright © 2018 Kevin Willocks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
