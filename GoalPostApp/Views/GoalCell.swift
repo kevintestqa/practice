1b9a5b17aa96aa22fcc98e9a5704a6a96001e535
Kevin Willocks <kevinwillocks@gmail.com>
2018-02-21 20:09:16 -0500

    Added GoalCell file in addition to IBoutlets

---
+++ GoalPostApp/Views/GoalCell.swift
@@ -0,0 +1,24 @@
+//
+//  GoalCell.swift
+//  GoalPostApp
+//
+//  Created by Kevin Willocks on 2/21/18.
+//  Copyright © 2018 Kevin Willocks. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
