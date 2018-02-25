cb4cd13067f05d0ffb553a7dccd89a9a841e9cf5
Kevin Willocks <kevinwillocks@gmail.com>
2018-02-24 21:29:18 -0500

    Re added stack views and re-added IBoutlets

--- GoalPostApp/Views/GoalCell.swift
+++ GoalPostApp/Views/GoalCell.swift
@@ -10,15 +10,16 @@ import UIKit
 
 class GoalCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    @IBOutlet weak var goalDescriptionLabel: UILabel!
+    
+    @IBOutlet weak var goalTypeLabel: UILabel!
+    
+    @IBOutlet weak var goalProgressLabel: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int ) {
+        self.goalDescriptionLabel.text = description
+        self.goalTypeLabel.text = type
+        self.goalProgressLabel.text = String(describing: goalProgressAmount)
+}
 
 }
