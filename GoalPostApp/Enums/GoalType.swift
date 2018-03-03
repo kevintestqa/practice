61d8d29dea2ba9e451393e5b9521509655c35618
Kevin Willocks <kevinwillocks@gmail.com>
2018-03-03 18:45:32 -0500

    Conformed to appropriate  protocols and created the GoalType Enum.  Need to continue from 6:42 in the video

---
+++ GoalPostApp/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  goalType.swift
+//  GoalPostApp
+//
+//  Created by Kevin Willocks on 3/3/18.
+//  Copyright © 2018 Kevin Willocks. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType {
+   case longTerm
+   case shortTerm
+}
